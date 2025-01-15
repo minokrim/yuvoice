a813041f242b8b38079c888983b2fc3655fd22f9
ayomide <ayomidekareem563@gmail.com>
2025-01-14 18:03:34 -0800

    indiv storu

---
+++ src/editorial/story/storycontent.jsx
@@ -0,0 +1,32 @@
+import  React from "react";
+export default function StoryContent({currentStory}){
+    const addTailwindClasses = (htmlContent) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(htmlContent, 'text/html');
+      
+        const paragraphs = doc.querySelectorAll('p');
+        paragraphs.forEach((p) => {
+          p.classList.add('mt-0','pt-0',"color-black")
+        });
+
+        const images = doc.querySelectorAll('img');
+        images.forEach((p) => {
+          p.classList.add('w-[60em]','h-[35em]','justify-self-center','self-center')
+        });
+      
+        return doc.body.innerHTML;
+      };
+      const editedContent = addTailwindClasses(currentStory.content.rendered);
+
+    return <main className="flex flex-col items-center">
+        <h5>
+        Editor's Note: Yuvoice uplifts diverse voices around the world. We focus on the perspectives of real people living through history and how Planet Earth looks through their eyes. We never necessarily endorse, promote, or agree with the pieces we publish. We want to showcase viewpoints of all types. Please check out our Statement of Global Progress for further information on our stance.
+        </h5>
+        <section dangerouslySetInnerHTML={{__html: editedContent,}} className="flex flex-col p-20 items-start text-left font-light text-base obj br w-[70%]">
+        </section>
+        
+        <section className="flex w-full items-center justify-center">
+            <p className="br flex w-full items-center justify-center bg-purple-600/20 rounded-lg">{currentStory.acf.topic_tags}</p>
+        </section>
+        </main>
+}
