9b18f9220371a340e0a17ad016dbb4e566a84d6e
ayomide <ayomidekareem563@gmail.com>
2025-01-26 18:31:44 -0800

    storycont

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HashRouter,Routes,Route} from 'react-router-dom';
 import EditorialBody from './editorial/editorialbody.jsx';
 import SubmitStoryBody from './editorial/submitstory/submitstorybody.jsx';
 import LandingBody from './landingpage/landingBody.jsx';
+import StoryBody from "../src/editorial/story/storybody.jsx"
 function App() {
   return (
     <div className="App">
@@ -14,7 +15,7 @@ function App() {
         <Route path='/' element={<LandingBody/>}/>
         <Route path='/globalstories' element={<EditorialBody/>}/>
         <Route path='/submitstory' element={<SubmitStoryBody/>}/>
-        {/* <Route path='/story/:storyId' element={<StoryBody/>}/> */}
+        <Route path='/story/:storyId' element={<StoryBody/>}/>
         </Routes>
       </ResponseProvider>
       </HashRouter>
