94ec7fa79ae7dfde5eced83d3fc337f0c2eeca05
ayomide <ayomidekareem563@gmail.com>
2025-01-27 20:23:43 -0800

    stories

--- src/editorial/story/storycontent.jsx
+++ src/editorial/story/storycontent.jsx
@@ -6,7 +6,7 @@ export default function StoryContent({currentStory}){
       
         const paragraphs = doc.querySelectorAll('p');
         paragraphs.forEach((p) => {
-          p.classList.add('mt-0','pt-0',"color-black")
+          p.classList.add('mt-0','pt-0',"color-black") 
         });
 
         const images = doc.querySelectorAll('img');
@@ -18,9 +18,9 @@ export default function StoryContent({currentStory}){
       };
       const editedContent = addTailwindClasses(currentStory.content.rendered);
 
-    return <main className="flex flex-col items-center">
-        <h5>
-        Editor's Note: Yuvoice uplifts diverse voices around the world. We focus on the perspectives of real people living through history and how Planet Earth looks through their eyes. We never necessarily endorse, promote, or agree with the pieces we publish. We want to showcase viewpoints of all types. Please check out our Statement of Global Progress for further information on our stance.
+    return <main className="flex flex-col items-center border border-solid border-red-500 w-[80%] justify-center">
+        <h5 className="text-gray-400 w-[49%] font-normal">
+        <b>Editor's Note:</b> Yuvoice uplifts diverse voices around the world. We focus on the perspectives of real people living through history and how Planet Earth looks through their eyes. We never necessarily endorse, promote, or agree with the pieces we publish. We want to showcase viewpoints of all types. Please check out our Statement of Global Progress for further information on our stance.
         </h5>
         <section dangerouslySetInnerHTML={{__html: editedContent,}} className="flex flex-col p-20 items-start text-left font-light text-base obj br w-[70%]">
           
