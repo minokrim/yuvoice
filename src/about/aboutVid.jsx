f3e87bd47286b1d93cee160d1e5f93e6f0c826c4
ayomide <ayomidekareem563@gmail.com>
2025-07-22 18:57:53 -0700

    working v1

--- src/about/aboutVid.jsx
+++ src/about/aboutVid.jsx
@@ -1,7 +1,7 @@
 export default function AboutVid(){
     return <main className="box-border mt-[-7.3em] md:mt-[-10em]">
                 <video className="relative w-[100%] h-[100%] " autoPlay loop muted >
-            <source src={`${process.env.PUBLIC_URL}/videos/avoutvid.mp4`} type="video/mp4" className=""/>
-        </video>
+                    <source src="https://firebasestorage.googleapis.com/v0/b/zendest-52099.firebasestorage.app/o/Videos%2Ffreecompress-8088943-uhd_4096_2160_25fps.mp4?alt=media&token=057a77ba-c32f-401d-8e66-2761aa69cc5d" type="video/mp4"/>        
+                    </video>
     </main>
 }
